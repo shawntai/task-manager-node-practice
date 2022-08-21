707006529095d893e3e86a000d580b95a371a7a6
shawntai <shawn890715@gmail.com>
2022-08-21 14:48:44 +0800

    set up the five requests

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const { mongodb_username, mongodb_password } = require("./config");
+const Task = require("./models/task");
+const uri = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.zfbgh.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: "TaskManagerApp",
+});
+// .then((result) => console.log(result));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/tasks", (req, res) => {
-	console.log("requst came in");
-	res.json({ success: true });
+	Task.find().then((result) => res.json({ success: true, result }));
 });
 
 app.get("/api/tasks/:id", (req, res) => {
-	console.log("requst came in");
-	res.json({ success: true });
+	Task.findById(req.params.id)
+		.then((result) => res.json({ success: true, result }))
+		.catch((error) => res.json({ error }));
 });
 
 app.post("/api/tasks", (req, res) => {
-	console.log("requst came in");
-	res.json({ success: true });
+	const newTask = new Task(req.body);
+	newTask.save().then((_) => res.send("POST request suceessful"));
 });
 
 app.put("/api/tasks/:id", (req, res) => {
-	console.log("requst came in");
-	res.json({ success: true });
+	Task.findByIdAndUpdate(req.params.id, req.body)
+		.then((result) => res.json({ success: true, result }))
+		.catch((error) => res.json({ error }));
 });
 
 app.delete("/api/tasks/:id", (req, res) => {
-	console.log("requst came in");
-	res.json({ success: true });
+	Task.findByIdAndDelete(req.params.id)
+		.then((result) => res.json({ success: true, result }))
+		.catch((error) => res.json({ error }));
 });
 
 app.listen(5001, (req, res) => {
