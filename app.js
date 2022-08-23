71cecf5b14369021906e8e9e0c84658f3cdda898
shawntai <shawn890715@gmail.com>
2022-08-23 09:40:36 +0800

    add env variable

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const { mongodb_username, mongodb_password } = require("./config");
-const Task = require("./models/task");
 const taskRouter = require("./routes/taskRoutes");
-const uri = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.zfbgh.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	dbName: "TaskManagerApp",
-});
+const Task = require("./models/task");
+require("dotenv").config();
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/tasks", taskRouter);
 
-app.listen(5001, (req, res) => {
-	console.log("listening on port 5001");
-});
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		dbName: "TaskManagerApp",
+	})
+	.then(() => {
+		app.listen(5001, (req, res) => {
+			console.log("listening on port 5001");
+		});
+	})
+	.catch((err) => {
+		console.log("Failed to connect to MongoDB", err);
+	});
